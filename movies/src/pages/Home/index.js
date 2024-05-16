a13dd30ab5e1498f491a1fdcf66288e303e847fe
Judson Ciribelli <judson.ciribelli@discente.ufma.br>
2024-05-16 16:07:15 -0300

    Err

--- movies/src/pages/Home/index.js
+++
@@ -1,8 +0,0 @@
-function Home() {
-  return (
-    <div>
-      <h2>Bem vindo a pagina Home</h2>
-    </div>
-  );
-}
-export default Home;
--- movies/src/pages/Movies/index.js
+++
@@ -1,8 +0,0 @@
-function Movies() {
-  return (
-    <div>
-      <h2>Voce esta na pagina dos filmes</h2>
-    </div>
-  );
-}
-export default Movies;
