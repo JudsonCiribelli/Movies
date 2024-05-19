736afdff5676e429a692ef71e8ee8a6c38202c42
Judson Ciribelli <judson.ciribelli@discente.ufma.br>
2024-05-19 00:19:35 -0300

    Importando nossa biblioteca Axios

--- catalogo/src/services/api.js
+++ catalogo/src/services/api.js
@@ -1,2 +1,3 @@
+import axios from "axios";
 // BASE DA URL: https://api.themoviedb.org/3
 // URL DA API https://api.themoviedb.org/3/movie/now_playing?api_key=7aacaa244c0ed846864d6b93f8125057&language=pt-BR
