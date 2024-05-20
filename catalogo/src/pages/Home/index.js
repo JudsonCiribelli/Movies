8a3cca97586b0c87a564d5278c4c4453ae040a76
Judson Ciribelli <judson.ciribelli@discente.ufma.br>
2024-05-20 10:24:48 -0300

    Estilizando nossa galeria de filmes

--- catalogo/src/pages/Home/index.js
+++ catalogo/src/pages/Home/index.js
@@ -1,11 +1,12 @@
-import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 import api from "../../services/api";
+import Movies from "../Movies";
+import "./home.css";
+import { Link } from "react-router-dom";
 //URL DA API https://api.themoviedb.org/3/movie/now_playing?api_key=7aacaa244c0ed846864d6b93f8125057&language=pt-BR
 
 function Home() {
   const [filmes, setFilmes] = useState([]);
-
   useEffect(() => {
     async function loadfilmes() {
       const response = await api.get("movie/now_playing", {
@@ -15,16 +16,28 @@ function Home() {
           page: 1,
         },
       });
-      console.log(response.data);
+      //console.log(response.data.results.slice(0, 10));
+      setFilmes(response.data.results.slice(0, 10));
     }
     loadfilmes();
   }, []);
 
   return (
-    <div>
-      <h2>Pagina Home</h2>
-      <Link to="/">Primeira pagina</Link> <br></br>
-      <Link to="/Movies">Movies</Link>
+    <div className="container">
+      <div className="list-movies">
+        {filmes.map((filme) => {
+          return (
+            <article key={filme.id}>
+              <strong>{filme.title}</strong>
+              <img
+                src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`}
+                alt={filme.title}
+              />
+              <Link to={`/filmes/${Movies.id}`}>Acessar</Link>
+            </article>
+          );
+        })}
+      </div>
     </div>
   );
 }
