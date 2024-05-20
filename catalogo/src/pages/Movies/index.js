11d3fda649e23cb592edfb512edf05cfef540f24
Judson Ciribelli <judson.ciribelli@discente.ufma.br>
2024-05-20 12:43:09 -0300

    Estilização da página sobre os detalhes do filmee

--- catalogo/src/pages/Movies/index.js
+++ catalogo/src/pages/Movies/index.js
@@ -1,10 +1,59 @@
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import api from "../../services/api";
+import "../Movies/movie.css";
 
 function Movies() {
+  const { id } = useParams();
+  const [movie, setMovie] = useState({});
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function loadFilme() {
+      await api
+        .get(`/movie/${id}`, {
+          params: {
+            api_key: "28fc232cc001c31e8a031f419d0a14ca",
+            language: "pt-BR",
+          },
+        })
+        .then((response) => {
+          setMovie(response.data);
+          setLoading(false);
+        })
+        .catch(() => {
+          console.log("FILME NAO ENCONTRADO");
+        });
+    }
+
+    loadFilme();
+
+    return () => {
+      console.log("COMPONENTE FOI DESMONTADO");
+    };
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="filme-info">
+        <h1>Carregando detalhes...</h1>
+      </div>
+    );
+  }
+
   return (
-    <div>
-      <h2>Sobre</h2> <br></br>
+    <div className="main">
+      <div className="container1">
+        <img
+          src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
+          alt={movie.title}
+        />
+      </div>
+      <div className="container2">
+        <h1>{movie.title}</h1>
+        <span>{movie.overview}</span>
+        <strong>Avaliação: {movie.vote_average} /10</strong>
+      </div>
     </div>
   );
 }
