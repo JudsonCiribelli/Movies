963a21fb0190c9a468f01e4df146bce6a68f5e8c
Judson Ciribelli <judson.ciribelli@discente.ufma.br>
2024-05-21 12:02:01 -0300

    Função para excluir algum filme da nossa lista

--- catalogo/src/pages/favoritos/index.js
+++ catalogo/src/pages/favoritos/index.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import "../favoritos/favoritos.css";
-import { Link } from "react-router-dom";
+import { Link, json } from "react-router-dom";
 
 function Favoritos() {
   const [filmes, setFilmes] = useState([]);
@@ -9,9 +9,20 @@ function Favoritos() {
     setFilmes(JSON.parse(minhaLista) || []);
   }, []);
 
+  function Excluir(id) {
+    let filtroFilmes = filmes.filter((item) => {
+      return item.id !== id;
+    });
+    setFilmes(filtroFilmes);
+    localStorage.setItem("@Prime", JSON.stringify(filtroFilmes));
+  }
+
   return (
     <div className="Movies">
       <h1>Meus filmes</h1>
+
+      {filmes.length === 0 && <span>Você não possui nenhum filme salvo</span>}
+
       <ul>
         {filmes.map((item) => {
           return (
@@ -19,7 +30,7 @@ function Favoritos() {
               <span>{item.title}</span>
               <div>
                 <Link to={`/Movies/${item.id}`}>Ver detalhes</Link>
-                <button>Excluir</button>
+                <button onClick={() => Excluir(item.id)}>Excluir</button>
               </div>
             </li>
           );
